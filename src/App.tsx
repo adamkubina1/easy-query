faffbbf2d6f5b433dfee9e9df8d8eea058b2d9b2
Adam Kubina <adam.kubinaa@gmail.com>
2023-09-27 16:21:56 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Box, Heading } from '@chakra-ui/react';
+import './App.css';
+
+function App() {
+  return (
+    <Box>
+      <Heading>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque enim
+        recusandae nesciunt, quos dolores cum doloribus qui eligendi aspernatur
+        nulla exercitationem non id optio quisquam veritatis, hic itaque?
+        Consequatur, incidunt.
+      </Heading>
+    </Box>
+  );
+}
+
+export default App;
